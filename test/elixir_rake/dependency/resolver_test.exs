680677c6c0e15d38627eb92d494e7a3c08f0e173
Yuri Hurski <hi1sailor@gmail.com>
2023-02-01 00:37:50 +0300

    Move dependency.ex to dependency/resolver.ex,      dependency_test.exs to dependeny/resolver_test.exs

---
+++ test/elixir_rake/dependency/resolver_test.exs
@@ -0,0 +1,38 @@
+defmodule ElixirTest.Dependency.ResolverTest do
+  use ExUnit.Case, async: true
+
+  doctest ElixirRake
+
+  import ElixirRake.Dependency.Resolver
+
+  test "simple one level flat dependencies" do
+    deps = [a: [], b: [], c: []]
+    result = [a: [], b: [], c: []]
+
+    assert flatten(deps) == result
+  end
+
+  test "returns empty kw list if empty kw list is passed in" do
+    assert flatten([]) == []
+  end
+
+  test "raises ArgumentError if argument is not a kw list" do
+    assert_raise ArgumentError, fn -> flatten([{:a, [:aa1, :aa2]}, :b]) end
+  end
+
+  test "simple two level deep dependencies" do
+    deps = [
+      a: [:kk, :kk2],
+      top: [:a, :b],
+      top2: [:top]
+    ]
+
+    result = [
+      a: [:kk, :kk2],
+      top: [{:a, [:kk, :kk2]}, :b],
+      top2: [top: [{:a, [:kk, :kk2]}, :b]]
+    ]
+
+    assert flatten(deps) == result
+  end
+end
