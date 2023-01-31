dcc3723fa3ca0b9eb2d6d53ece650be357390ad7
Yuri Hurski <hi1sailor@gmail.com>
2023-01-31 03:23:26 +0300

    Add tests for dependency/formatter.ex

--- test/elixir_rake/dependency/formatter_test.exs
+++ test/elixir_rake/dependency/formatter_test.exs
@@ -5,15 +5,55 @@ defmodule ElixirTest.Dependency.FormatterTest do
 
   import ElixirRake.Dependency.Formatter
 
+  # setup_all do
+  #   module = Asdf
+
+  #   :ok
+  # end
+
   test "empty dependency list expands to empty list" do
     assert expand_each([], TaskMod) == []
   end
 
-  test "[:dep] dependency expands to [{module, atom, []}]" do
+  test "[:dep] dependency expands to [{module, :dep, []}]" do
     assert expand_each([:dep], TaskMod) == [{TaskMod, :dep, []}]
   end
 
-  test "[{:dep, []}] dependency expands to [{module, atom, []}]" do
-    assert expand_each([{:dep, []}], TaskMod) == [{TaskMod, :dep, []}]
+  test "[:dep1, :dep2, :dep3] dependency expands to \
+        [{module, :dep1, []}, {module, :dep2, []}, {module, :dep3, []}]" do
+    deps = [:dep1, :dep2, :dep3]
+
+    result = [
+      {TaskMod, :dep1, []},
+      {TaskMod, :dep2, []},
+      {TaskMod, :dep3, []}
+    ]
+
+    assert expand_each(deps, TaskMod) == result
+  end
+
+  test "[{:dep, []}] dependency expands to [{module, :dep, []}]" do
+    deps = [{:dep, []}]
+    result = [{TaskMod, :dep, []}]
+
+    assert expand_each(deps, TaskMod) == result
+  end
+
+  test "[{:dep1, []}, {:dep2, []}, {:dep3, []}] dependency expands to \
+        [{module, :dep1, []}, {module, :dep2, []}, {module, :dep3, []}]" do
+    module = TaskMod
+    deps = [{:dep1, []}, {:dep2, []}, {:dep3, []}]
+    result = [{module, :dep1, []}, {module, :dep2, []}, {module, :dep3, []}]
+
+    assert expand_each(deps, module) == result
+  end
+
+  test "[{ext_module, :dep}] dependency expands to [{ext_module, :dep, []}]" do
+    task_module = TaskMod
+    ext_module = ExternalMod
+    deps = [{ext_module, :dep}]
+    result = [{ext_module, :dep, []}]
+
+    assert expand_each(deps, TaskMod) == result
   end
 end
