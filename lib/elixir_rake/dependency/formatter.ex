f9505b97a73e5d7bd8dd456e370051b1e729df9c
Yuri Hurski <hi1sailor@gmail.com>
2023-01-31 21:43:34 +0300

    Add more tests for dependency/formatter.ex

--- lib/elixir_rake/dependency/formatter.ex
+++ lib/elixir_rake/dependency/formatter.ex
@@ -1,12 +1,16 @@
 defmodule ElixirRake.Dependency.Formatter do
   def expand_each(deps_list, task_module), do: expand_each(deps_list, [], task_module)
-  defp expand_each([], expanded_deps_list, _task_module), do: expanded_deps_list
+  defp expand_each([], expanded_deps_list, _task_module) do
+     List.flatten(expanded_deps_list) |> Enum.reverse()
+  end
   defp expand_each([h | t], expanded_deps_list, task_module) do
-    [expand(task_module, h) | expand_each(t, expanded_deps_list, task_module)]
+    expand_each(t, [expand(task_module, h) | expanded_deps_list], task_module)
   end
 
-  # defp expand([]), do: []
   defp expand(mod, list) when is_list(list), do: expand_each(list, mod)
+  defp expand(mod, {func}) when is_atom(func) do # depends_on({:ping})
+    expand(mod, func)
+  end
   defp expand(mod, func) when is_atom(func) do # depends_on(:ping)
     {mod, func, []}
   end
--- test/elixir_rake/dependency/formatter_test.exs
+++ test/elixir_rake/dependency/formatter_test.exs
@@ -19,6 +19,23 @@ defmodule ElixirTest.Dependency.FormatterTest do
     assert expand_each([:dep], TaskMod) == [{TaskMod, :dep, []}]
   end
 
+  test "[[:dep]] dependency expands to [{task_module, :dep, []}]" do
+    assert expand_each([:dep], TaskMod) == [{TaskMod, :dep, []}]
+  end
+
+  test "[{:dep}] dependency expands to [{task_module, :dep, []}]" do
+    # IO.inspect expand_each([[{:dep, :dep2}]], TaskMod)
+    assert expand_each([:dep], TaskMod) == [{TaskMod, :dep, []}]
+  end
+
+  test "[{:dep, :dep2, :dep3}] raises FunctionClauseError" do
+    assert_raise FunctionClauseError, fn -> expand_each([{:dep, :dep2, :dep3}], TaskMod) end
+  end
+
+  test "[[{:dep, :dep2, :dep3}]] raises FunctionClauseError" do
+    assert_raise FunctionClauseError, fn -> expand_each([[{:dep, :dep2, :dep3}]], TaskMod) end
+  end
+
   test "[:dep1, :dep2, :dep3] dependency expands to \
         [{task_module, :dep1, []}, {task_module, :dep2, []}, {task_module, :dep3, []}]" do
     deps = [:dep1, :dep2, :dep3]
@@ -33,6 +50,13 @@ defmodule ElixirTest.Dependency.FormatterTest do
   end
 
   test "[{:dep, []}] dependency expands to [{task_module, :dep, []}]" do
+    deps = [{:dep, []}]
+    result = [{TaskMod, :dep, []}]
+
+    assert expand_each(deps, TaskMod) == result
+  end
+
+  test "[[{:dep, []}]] dependency expands to [{task_module, :dep, []}]" do
     deps = [{:dep, []}]
     result = [{TaskMod, :dep, []}]
 
