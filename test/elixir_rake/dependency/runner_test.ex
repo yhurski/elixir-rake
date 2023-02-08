8dd67a7ff76ecbbc283ce4ee75dcfb3d2b286dd4
Yuri Hurski <hi1sailor@gmail.com>
2023-02-08 15:13:00 +0300

    Add dependency/runner.ex test

--- test/elixir_rake/dependency/runner_test.ex
+++ test/elixir_rake/dependency/runner_test.ex
@@ -3,6 +3,14 @@ defmodule ElixirTest.Dependency.RunnerTest do
 
   doctest ElixirRake
 
+  setup_all do
+    defmodule DepMod1 do
+      def raise_exception(type, msg), do: raise type, msg
+    end
+
+    {:ok, dep_mod1: DepMod1}
+  end
+
   describe "Runner.run/2" do
     import ElixirRake.Dependency.Runner
 
@@ -14,6 +22,16 @@ defmodule ElixirTest.Dependency.RunnerTest do
       deps = [{Kernel, :+, [1, 2]}]
 
       assert run(deps) == 3
+    end
+
+    test "throws an exception outside of dependency chain", %{dep_mod1: dep_mod1} do
+      deps = [
+        {List, :duplicate, [[1], 1]},
+        {dep_mod1, :raise_exception, [ArgumentError, "test"]},
+        {List, :duplicate, [[2], 1]}
+      ]
+
+      assert_raise ArgumentError, fn -> run(deps) end
     end
   end
 end
