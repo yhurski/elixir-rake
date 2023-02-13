ddfe98b464657f121436932d60bae2fd97a86b63
Yuri Hurski <hi1sailor@gmail.com>
2023-02-13 23:09:19 +0300

    Add lib/elixir_rake.ex and test/elixir_rake_test.exs

--- lib/elixir_rake.ex
+++ lib/elixir_rake.ex
@@ -1,18 +1,60 @@
 defmodule ElixirRake do
-  @moduledoc """
-  Documentation for `ElixirRake`.
-  """
+  defmacro __using__(_opts) do
+    quote do
+      Module.register_attribute(__MODULE__, :elixir_rake_tasks_with_deps, [])
+      Module.put_attribute(__MODULE__, :elixir_rake_tasks_with_deps, [])
 
-  @doc """
-  Hello world.
+      @on_definition ElixirRake
+      @before_compile ElixirRake
+    end
+  end
 
-  ## Examples
+  def __on_definition__(env, _kind, name, args, _guards, _body) do
+    import ElixirRake.Dependency.Formatter
 
-      iex> ElixirRake.hello()
-      :world
+    description = Module.delete_attribute(env.module, :elixir_rake_desc)
+    dependencies = Module.delete_attribute(env.module, :elixir_rake_task_deps)
+    global_deps = Module.get_attribute(env.module, :elixir_rake_tasks_with_deps)
+    mod = env.module
 
-  """
-  def hello do
-    :world
+    if description do
+      if dependencies do
+        Module.put_attribute(
+          env.module,
+          :elixir_rake_tasks_with_deps,
+          [{{mod, name, length(args)}, description, expand_each(dependencies, mod)} | global_deps]
+        )
+      else
+        Module.put_attribute(
+          env.module,
+          :elixir_rake_tasks_with_deps,
+          {name, description, [{{mod, name, args}, description, []} | global_deps]}
+        )
+      end
+    end
+  end
+
+  defmacro __before_compile__(env) do
+    import ElixirRake.Dependency.Resolver
+
+    tasks_with_deps = Module.get_attribute(env.module, :elixir_rake_tasks_with_deps)
+    IO.inspect [">>>", tasks_with_deps]
+
+    quote do
+      def __elixir_rake_tasks_deps__(), do: unquote(Macro.escape(resolve(tasks_with_deps)))
+    end
+  end
+
+  defmacro desc(text) do
+    quote do
+      Module.put_attribute(__MODULE__, :elixir_rake_desc, unquote(text))
+    end
+  end
+
+  defmacro depends_on(args) do
+    quote do
+      Module.register_attribute(__MODULE__, :elixir_rake_task_deps, accumulate: true)
+      Module.put_attribute(__MODULE__, :elixir_rake_task_deps, unquote(args))
+    end
   end
 end
--- test/elixir_rake_test.exs
+++ test/elixir_rake_test.exs
@@ -2,6 +2,22 @@ defmodule ElixirRakeTest do
   use ExUnit.Case
   doctest ElixirRake
 
+  setup_all do
+    defmodule TaskMod1 do
+      use ElixirRake
+      import ElixirRake
+
+      desc "task1"
+      depends_on [:task2, {IO, :puts, ["from IO"]}]
+      def task1(), do: IO.puts("test task 1")
+
+      desc "task2"
+      def task2(), do: IO.puts("test task 2")
+    end
+
+    {:ok, task_mod1: TaskMod1}
+  end
+
   describe "ElixirRake module" do
     test "has __using__ macro" do
       assert macro_exported?(ElixirRake, :__using__, 1)
@@ -25,18 +41,51 @@ defmodule ElixirRakeTest do
   end
 
   describe "Task module" do
-    setup do
-      defmodule TaskMod do
-        use ElixirRake
-        import ElixirRake
-      end
-
-      {:ok, task_mod: TaskMod}
+    test "has __elixir_rake_tasks_deps__/0 function", %{task_mod1: task_mod1} do
+      assert function_exported?(task_mod1, :__elixir_rake_tasks_deps__, 0)
     end
 
-    test "", %{task_mod: task_mod} do
-      # IO.inspect context
-      assert ElixirRake.hello() == :world
+    test "has task1/0 in __elixir_rake_tasks_deps__/0 function", %{task_mod1: task_mod1} do
+      assert List.keymember?(task_mod1.__elixir_rake_tasks_deps__(), {task_mod1, :task1, 0}, 0)
+    end
+
+    test "has task2/0 in __elixir_rake_tasks_deps__/0 function", %{task_mod1: task_mod1} do
+      assert List.keymember?(task_mod1.__elixir_rake_tasks_deps__(), {task_mod1, :task2, 0}, 0)
+    end
+
+    test "has description for task1/0 in __elixir_rake_tasks_deps__/0 function", %{task_mod1: task_mod1} do
+      with item <- List.keyfind(task_mod1.__elixir_rake_tasks_deps__(), {task_mod1, :task1, 0}, 0) do
+        assert elem(item, 1) == "task1"
+      end
+    end
+
+    test "has description for task2/0 in __elixir_rake_tasks_deps__/0 function", %{task_mod1: task_mod1} do
+      with item <- List.keyfind(task_mod1.__elixir_rake_tasks_deps__(), {task_mod1, :task2, 0}, 0) do
+        assert elem(item, 1) == "task2"
+      end
+    end
+
+    test "has two deps for task1/0 in __elixir_rake_tasks_deps__/0 function", %{task_mod1: task_mod1} do
+      deps = [
+        {IO, :puts, ["from IO"]},
+        {ElixirRakeTest.TaskMod1, :task2, []}
+      ]
+
+      with item <- List.keyfind(task_mod1.__elixir_rake_tasks_deps__(), {task_mod1, :task1, 0}, 0) do
+        assert elem(item, 2) == deps
+      end
+    end
+
+    test "has zero deps for task2/0 in __elixir_rake_tasks_deps__/0 function", %{task_mod1: task_mod1} do
+      with item <- List.keyfind(task_mod1.__elixir_rake_tasks_deps__(), {task_mod1, :task2, 0}, 0) do
+        assert elem(item, 2) == []
+      end
+    end
+
+    test "imports desc macro", %{task_mod1: task_mod1} do
+      IO.inspect task_mod1.__elixir_rake_tasks_deps__()
+
+      assert task_mod1.task1() == :ok
     end
   end
 end
